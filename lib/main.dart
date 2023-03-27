45088a5ec90152353b4b34c1a8a7d865b09387f0
RAFAEL_ALI_FONSECA_ALFARO <a.20308051280610@cbtis128.edu.mx>
2023-03-27 18:01:10 +0000

    practica5TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,68 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Pestaña tab',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        // This is the theme of your application.
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(50), // Creates border
+                color: Color(0xff5f5e54)), // Change background color from here
+            tabs: [
+              Tab(
+                icon: Icon(Icons.shopping_cart, color: Colors.yellow),
+                text: 'Productos',
+              ),
+              Tab(
+                icon: Icon(Icons.card_giftcard, color: Colors.yellow),
+                text: 'Ofertas',
+              ),
+              Tab(
+                icon: Icon(Icons.local_shipping, color: Colors.yellow),
+                text: 'Envío',
+              ),
+              Tab(
+                icon: Icon(Icons.help_outline, color: Colors.yellow),
+                text: 'Ayuda',
+              ),
+            ],
+          ),
+          backgroundColor: Colors.black, // Change the color of the app bar
+          title: Text('Tabs Demo', style: TextStyle(color: Colors.yellow)),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.shopping_cart, size: 350, color: Color(0xff5f5e54)),
+            Icon(Icons.card_giftcard, size: 350, color: Color(0xff5f5e54)),
+            Icon(Icons.local_shipping, size: 350, color: Color(0xff5f5e54)),
+            Icon(Icons.help_outline, size: 350, color: Color(0xff5f5e54)),
+          ],
         ),
       ),
     );
